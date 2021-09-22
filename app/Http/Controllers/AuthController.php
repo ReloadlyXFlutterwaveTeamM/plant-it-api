9503c0ca355fb14d752d222e63cc73b27d01d678
Tolulope Adekunte <toluadekunte@gmail.com>
2021-09-22 07:27:01 +0100

    Auth

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
+use Validator;
+
+class AuthController extends Controller
+{
+    public function signup(Request $request){
+
+        	$validator = Validator::make($request->all(), [
+                'name' => 'required|string',
+                'phone' => 'required|min:11|max:11',
+                'email' => 'required|string|unique:users,email',
+                'password' => 'required|string',
+                'terms' => 'required|numeric'
+
+            ]);        
+
+         if (!$validator->fails()){
+
+            $data=$request->all();
+
+            $user = User::create([
+                'name' => $data['name'],
+                'phone' => $data['phone'],
+                'email' => $data['email'],
+                'password' => bcrypt($data['password']),
+                'terms' => $data['terms']
+            ]);
+
+            $token = $user->createToken('usertoken')->plainTextToken;
+
+            $response = [
+                'user' => $user,
+                'token' => $token
+
+            ];
+
+            return response($response, 201);
+        
+        }else{
+
+            return $validator->errors();
+
+        }
+}
+
+
+    public function signin(Request $request){
+
+    $validator = Validator::make($request->all(), [
+        'email' => 'required|string',
+        'password' => 'required|string'
+        ]);        
+
+     if (!$validator->fails())
+       {
+
+        $data=$request->all();
+
+        // Check email
+        $user = User::where('email', $data['email'])->first();
+
+        // Check password
+        if(!$user || !Hash::check($data['password'], $user->password)) {
+            return response([
+                'message' => 'Email or Password is incorrect'
+            ], 401);
+        }
+
+        $token = $user->createToken('myapptoken')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+
+        return response($response, 201);
+    }else{
+        return $validator->errors();
+    }
+    
+    }
+
+    public function logout(){
+        auth()->user()->tokens()->delete();
+
+        return [
+            'message' => 'Logged out'
+        ];
+    }
+}
